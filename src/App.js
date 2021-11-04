5f4f1b1ea8d6af9276edb1b7c2e6a844775a4104
TheRealPatronum <ragnar@patronum.se>
2021-11-04 22:58:31 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { Cardlist } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -17,9 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <Cardlist name='Ragnar'>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </Cardlist>
       </div>
     );
   }
